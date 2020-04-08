70c85f330e6d4e9fc60f3470bf950a0f81513987
Thomas Pronold <tp@zotorn.de>
2020-04-08 10:30:41 +0200

    Init t3x_vuejs

---
+++ Classes/Controller/VueBackendController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace VUEJS\Vuejs\Controller;
+
+
+use TYPO3\CMS\Backend\View\BackendTemplateView;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
+
+class VueBackendController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+    /**
+     * Backend Template Container
+     *
+     * @var string
+     */
+    protected $defaultViewObjectName = \TYPO3\CMS\Backend\View\BackendTemplateView::class;
+
+    /**
+     * Load VUE.JS
+     *
+     * @param ViewInterface $view
+     * @return void
+     */
+    protected function initializeView(ViewInterface $view)
+    {
+        if (!($this->view instanceof BackendTemplateView)) {
+            return;
+        }
+
+        /** @var BackendTemplateView $view */
+        parent::initializeView($view);
+
+        $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
+
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['BE']['debug']) && $GLOBALS['TYPO3_CONF_VARS']['BE']['debug']) {
+            $pageRenderer->addJsFile('EXT:vuejs/Resources/Public/JavaScript/Contrib/Vue/vue.js'); // development version with hints
+        } else {
+            $pageRenderer->addJsFile('EXT:vuejs/Resources/Public/JavaScript/Contrib/Vue/vue.min.js'); // silent production version
+        }
+    }
+}
---
+++ ext_emconf.php
@@ -0,0 +1,26 @@
+<?php
+
+$EM_CONF['vuejs'] = array(
+    'title' => 'VUE.JS Framework for TYPO3 Backend Modules',
+    'description' => 'VUE.JS Framework for TYPO3 Backend Modules',
+    'category' => 'be',
+    'author' => 'Thomas Pronold',
+    'author_email' => 'tp@zotorn.de',
+    'author_company' => 'Zotorn IT (zotorn.de)',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'clearCacheOnLoad' => false,
+    'version' => '2.6.11',
+    'constraints' =>
+        array(
+            'depends' =>
+                array(
+                    'typo3' => '9.5.0-9.5.99',
+                ),
+            'conflicts' =>
+                array(),
+            'suggests' =>
+                array(),
+        ),
+);
+
