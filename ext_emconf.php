7ee6f2d4d6fd377540fe71b9c80a5671bc36c94f
Thomas Pronold <tp@zotorn.de>
2020-04-10 18:25:49 +0200

    Set version to 1.0.3
    Compatability with TYPO LTS 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF['vuejs'] = array(
     'state' => 'stable',
     'uploadfolder' => false,
     'clearCacheOnLoad' => false,
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'constraints' =>
         array(
             'depends' =>
