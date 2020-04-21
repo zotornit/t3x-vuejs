88c79edc2e8e36dca9013deec3ab03b7ec9e21e6
Thomas Pronold <tp@zotorn.de>
2020-04-21 09:53:19 +0200

    Set version to 1.0.4
    Small adjustments and maintenance

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF['vuejs'] = array(
     'state' => 'stable',
     'uploadfolder' => false,
     'clearCacheOnLoad' => false,
-    'version' => '1.0.3',
+    'version' => '1.0.4',
     'constraints' =>
         array(
             'depends' =>
